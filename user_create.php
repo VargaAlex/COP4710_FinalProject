776e3462134e6baa22ca3543dd212b57d0b6d0f3
b0x-Cub3d <jcs_cubed2197@knights.ucf.edu>
2023-04-17 23:12:54 -0400

    Attempted to create University and SuperAdmin
    Might be missing a few small details

--- user_create.php
+++ user_create.php
@@ -1,10 +1,9 @@
 <?php
-
 include('config.php');
-
 ?>
 
 <?php
+// Create a new user
 $u_id; 
 $sql = "Select u_id from Users order by u_id DESC LIMIT 1";
     if ($res = mysqli_query($db,$sql)) {
@@ -13,29 +12,53 @@ $sql = "Select u_id from Users order by u_id DESC LIMIT 1";
     } else {
         echo "Error: " . $sql . "<br>" . $db->error;
     }
-
-
-// Retrieve the university information from the database
-$sql = "SELECT * FROM University";
-if ($res = mysqli_query($db,$sql)){
-    // Output the university information in a table
-    echo "<table>";
-    echo "<tr><th>ID</th><th>Name</th><th>Admin ID</th></tr>";
-    while ($row = $res->fetch_assoc()) {
-        echo "<tr><td>" . $row["uni_id"] . "</td><td>" . $row["uni_name"] . "</td><td>" . $row["sa_id"] . "</td></tr>";
-    }
-    echo "</table>";
-  } else {
-    echo "No groups found";
-  }
-  
-  $db->close();
 ?>
 
-<p>Create User</p>
-<form action="user_create.php" method="post">
-  <label for="University">University:</label>
-  <input type="text" id="university" name="university" required>
+<?php
+echo "
+<p>
+University: 
+<select name='uniSelect'>
+<option value=''>Select...</option>
+";
 
-  <input type="submit" value="Create University">
+// List out the universities
+$sql = "select uni_id, uni_name from univeristy";
+if($res = mysqli_query($db,$sql)) {
+   while($row = mysqli_fetch_array($res)) {
+    $uni_id = $row[0];
+    $uni_name = $row[1];
+    echo "
+    <option value=$l_id>$l_name</option>
+    ";
+  }
+}
+
+// Create University and SuperAdmin
+$sql = "insert into University(uni_name, info) values ('$uni_name', '$info') ";
+if($res = mysqli_query($db,$sql)) {
+    $sql = "select last_insert_id() ";
+    $uni_id;
+    $sa_id;
+      if($res = mysqli_query($db,$sql)) {
+        if($row = mysqli_fetch_array($res)) {
+          $uni_id = $row[0];
+          $sa_id = $row[3];
+        }
+      }
+      echo "$uni_id
+            $sa_id";
+}
+
+    echo "
+</select>
+</p>
+<input type = 'hidden' name = 'university' value = 'uniSelect' />
+";
+?>
+
+<p>If your university is not listed below, create it here</p>
+<form action="user_create.php" method="post">
+<label>University :</label></label><input type="text" id="university" name="university" required>
+<input type= "submit" value="Submit"/><br />
 </form>
