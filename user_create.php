89330cf75498338ac0d3ce7bb09988281a8efa34
b0x-Cub3d <jcs_cubed2197@knights.ucf.edu>
2023-04-18 16:20:43 -0400

    Update user_create.php

--- user_create.php
+++ user_create.php
@@ -4,22 +4,6 @@ include('config.php');
 
 <?php
 $error = "";
-    $uni_id;
-    // List out the universities
-    $sql = "select uni_id, uni_name from university";
-    if($res = mysqli_query($db,$sql)) {
-       while($row = mysqli_fetch_array($res)) {
-        $uni_id = $row[0];
-        $uni_name = $row[1];
-        echo "
-        <select data-label=\"Available Universities\">
-        <option value=\"\">-- Select University -- </option>
-          <option value=$uni_id>$uni_name</option>
-        </select>
-        ";
-      }
-    }
-
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = mysqli_real_escape_string($db,$_POST['username']);
     $pass = mysqli_real_escape_string($db,$_POST['password']);
@@ -43,19 +27,43 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
       }
     } 
 ?>
-
+<html>
 <form action="user_create.php" method = "post">
-  <h2>Create a new University? If so, enter below</h2>
-  <div>
-  <input type="radio" id="userSelect1" name="register"  value="Yes" />
-  <input type="radio" id="userSelect2" name="register"  value="No" />
-  <label>No </label>
-  </div>
-  <br><br>
+  <h1>Register User</h1>
   <label for="username">Username:</label>
   <input type="text" name="username" id="username" required><br>
 
   <label for="password">Password:</label>
   <input type="password" name="password" id="password" required><br>
-  <input type = "submit" value = " Register "/><br />
+
+  <h2>Create a new University? If so, enter below</h2>
+  <div>
+</html>
+
+<?php
+    $uni_id;
+    $sa_id;
+    // List out the universities
+    $sql = "select uni_id, uni_name from university";
+    if($res = mysqli_query($db,$sql)) {
+       while($row = mysqli_fetch_array($res)) {
+        $uni_id = $row[0];
+        $uni_name = $row[1];
+        echo "
+        <input type='radio' id='userSelect1' name='register'  value='Yes' />
+        <label>Yes </label><input type='text' id='university' name='$uni_name'>
+        <input type='radio' id='userSelect2' name='register'  value='No' />
+        <label>No </label>
+        <select data-label='Available Universities'>
+        <option value=''>-- Select University -- </option>
+          <option value=$uni_id>$uni_name</option>
+        </select>
+        </div>
+        <input type = 'submit' value = ' Register '/><br />
+        <input type = 'hidden' name = 'university' value = '$uni_name' />
+        ";
+      }
+    }
+?>
 </form>
+</html>
